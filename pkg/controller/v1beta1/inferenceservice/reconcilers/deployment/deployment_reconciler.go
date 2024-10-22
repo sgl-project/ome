f91982031b82e2b4346e772b936209603b9e109d
Beiwen Guo <beiwen.guo@oracle.com>
2024-10-22 21:25:25 +0000

    Update MaxUnavailable to 0 for Deployment resource
    Merge in GENAICORE/ome from update-max-unavailable to main
    
    Squashed commit of the following:
    
    commit da099c3f8aa6965517aa6b8365a82aea4a626eaf
    Author: Beiwen Guo <beiwen.guo@oracle.com>
    Date:   Tue Oct 22 12:54:03 2024 -0700
    
        Update MaxUnavailable to 0 for Deployment resource

--- pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
@@ -173,7 +173,7 @@ func setDefaultDeploymentSpec(spec *appsv1.DeploymentSpec) {
 	}
 	if spec.Strategy.Type == appsv1.RollingUpdateDeploymentStrategyType && spec.Strategy.RollingUpdate == nil {
 		spec.Strategy.RollingUpdate = &appsv1.RollingUpdateDeployment{
-			MaxUnavailable: &intstr.IntOrString{Type: intstr.Int, IntVal: 1},
+			MaxUnavailable: &intstr.IntOrString{Type: intstr.Int, IntVal: 0},
 			MaxSurge:       &intstr.IntOrString{Type: intstr.Int, IntVal: 1},
 		}
 	}
