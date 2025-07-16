256728eb5e34a1710937c58a03651ff3f32c1b16
MARATRIX <maratrix@163.com>
2025-07-16 14:13:32 +0800

    fix: 🔧  Missing pkg/version package. (#190)
    * feat: 🔧 Add the necessary version log information.
    
    * style: add some comments.

---
+++ pkg/version/version.go
@@ -0,0 +1,8 @@
+package version
+
+var (
+	// GitVersion is the git version of the build. It is set by the linker.
+	GitVersion = "unknown"
+	// GitCommit is the git commit hash of the build. It is set by the linker.
+	GitCommit = "unknown"
+)
