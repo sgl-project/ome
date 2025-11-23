fe70f600f59302416cc3c229318b879559c74d16
yifeliu <yifeng.liu@oracle.com>
2025-11-22 20:54:01 -0800

    [Bugfix] fix MergeNodeSelector with different componentType

--- pkg/controller/v1beta1/inferenceservice/components/engine.go
+++ pkg/controller/v1beta1/inferenceservice/components/engine.go
@@ -301,7 +301,7 @@ func (e *Engine) reconcilePodSpec(isvc *v1beta1.InferenceService, objectMeta *me
 		return nil, err
 	}
 	UpdatePodSpecVolumes(&e.BaseComponentFields, isvc, podSpec, objectMeta)
-	UpdatePodSpecNodeSelector(&e.BaseComponentFields, isvc, podSpec)
+	UpdatePodSpecNodeSelector(&e.BaseComponentFields, isvc, podSpec, v1beta1.EngineComponent)
 	UpdateEngineAffinity(&e.BaseComponentFields, isvc, podSpec)
 
 	e.Log.Info("Engine PodSpec updated", "inference service", isvc.Name, "namespace", isvc.Namespace)
@@ -334,7 +334,7 @@ func (e *Engine) reconcileWorkerPodSpec(isvc *v1beta1.InferenceService, objectMe
 		return nil, err
 	}
 	UpdatePodSpecVolumes(&e.BaseComponentFields, isvc, workerPodSpec, objectMeta)
-	UpdatePodSpecNodeSelector(&e.BaseComponentFields, isvc, workerPodSpec)
+	UpdatePodSpecNodeSelector(&e.BaseComponentFields, isvc, workerPodSpec, v1beta1.EngineComponent)
 	UpdateEngineAffinity(&e.BaseComponentFields, isvc, workerPodSpec)
 	e.Log.Info("Engine Worker PodSpec updated", "inference service", isvc.Name, "namespace", isvc.Namespace)
 	return workerPodSpec, nil
