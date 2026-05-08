2b3083ac8836af60c617090f07b8c1e8cc67a410
Arthur Cheng <arthur.cheng@oracle.com>
2026-05-08 12:48:53 -0700

    deprecate weight since matcher requires strict equality on model format and model architecture

--- pkg/runtimeselector/matcher.go
+++ pkg/runtimeselector/matcher.go
@@ -226,6 +226,7 @@ func (m *DefaultRuntimeMatcher) evaluateFormatMatch(model *v1beta1.BaseModelSpec
 				match.FormatMatch = false
 			}
 
+			// TODO: stop populating match.Weight; the field is deprecated and no longer read by the scorer.
 			if match.FormatMatch && format.ModelFormat.Weight > 0 {
 				match.Weight += format.ModelFormat.Weight * int64(match.Priority)
 			}
@@ -245,6 +246,7 @@ func (m *DefaultRuntimeMatcher) evaluateFormatMatch(model *v1beta1.BaseModelSpec
 				match.FrameworkMatch = false
 			}
 
+			// TODO: stop populating match.Weight; the field is deprecated and no longer read by the scorer.
 			if match.FrameworkMatch && format.ModelFramework.Weight > 0 {
 				match.Weight += format.ModelFramework.Weight * int64(match.Priority)
 			}
